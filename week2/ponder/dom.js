18ad83aa623c38373544ddbd31589b980283318f
Cassidy Whitehead <whi21049@byui.edu>
2026-04-25 18:07:05 -0600

    ponder week 2

---
+++ week2/ponder/dom.js
@@ -0,0 +1,34 @@
+// Practice with DOM manipulationg
+const title = document.querySelector('h1');
+
+title.textContent = 'Web Development is Fun!';
+
+let topics = document.querySelector('#topics');
+
+document.getElementById('topics').style.color = '#358d71';
+
+let list = document.querySelector('.list');
+
+list.style.border = '3px solid black';
+
+let para = document.querySelector('p');
+
+// para.style.backgroundColor = 'lightblue';
+
+para.classList.add('background');
+
+document.querySelector('body').classList.add('background');
+
+const image = document.querySelector('img');
+
+image.setAttribute('src', 'images/web.png');
+
+
+
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
+})
